8f887b98ce5c25b55d3a407b6f3cadc363e33855
Royalblood27 <emezy10@gmail.com>
2022-09-09 18:47:39 -0700

    paupau

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
  * main- Serves as the entry point for the program
- * Returns: By default return zero if no errors otherwise returns non-zero value*/
+ * Return: By default returns zero if no errors otherwise returns non-zero value*/
 int main (void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-return (0);
+        return (0);
 }
